88dbe8334b627e577841148e667da1c2b96a6e5b
Thomas Adams <thomas.adams@hutton.ac.uk>
2022-04-07 21:49:14 +0100

    Corrects api key

--- magic_8_ball/ask_the_ball.py
+++ magic_8_ball/ask_the_ball.py
@@ -15,7 +15,7 @@ def fetch_random_number():
         "jsonrpc": "2.0",
         "method": "generateIntegers",
         "params": {
-            "apiKey": "1luVRBATYPwOuTGR5W1iGhY39sMv8BTLQs1cqP4dnuEN1qJrEoZRBVdOlkveBQrIEsMhIVKV1F+GyvwGVX1Svw==",
+            "apiKey": "56edb62f-4122-4a64-bbd3-e17149ef925c",
             "n": 1,
             "min": 1,
             "max": 20,
@@ -43,46 +43,46 @@ def get_answer():
     number = fetch_random_number()
     match number:
         case "1":
-            answer = "It is certain."
+            string = "It is certain."
         case "2":
-            answer = "It is decidedly so."
+            string = "It is decidedly so."
         case "3":
-            answer = "Without a doubt."
+            string = "Without a doubt."
         case "4":
-            answer = "Yes definitely."
+            string = "Yes definitely."
         case "5":
-            answer = "You may rely on it."
+            string = "You may rely on it."
         case "6":
-            answer = "As I see it, yes."
+            string = "As I see it, yes."
         case "7":
-            answer = "Most likely."
+            string = "Most likely."
         case "8":
-            answer = "Outlook good."
+            string = "Outlook good."
         case "9":
-            answer = "Yes."
+            string = "Yes."
         case "10":
-            answer = "Signs point to yes."
+            string = "Signs point to yes."
         case "11":
-            answer = "Reply hazy, try again."
+            string = "Reply hazy, try again."
         case "12":
-            answer = "Ask again later."
+            string = "Ask again later."
         case "13":
-            answer = "Better not tell you now."
+            string = "Better not tell you now."
         case "14":
-            answer = "Cannot predict now."
+            string = "Cannot predict now."
         case "15":
-            answer = "Concentrate and ask again."
+            string = "Concentrate and ask again."
         case "16":
-            answer = "Don't count on it."
+            string = "Don't count on it."
         case "17":
-            answer = "My reply is no."
+            string = "My reply is no."
         case "18":
-            answer = "My sources say no."
+            string = "My sources say no."
         case "19":
-            answer = "Outlook not so good."
+            string = "Outlook not so good."
         case "20":
-            answer = "Very doubtful."
-    return(answer)
+            string = "Very doubtful."
+    return(string)
 
 # Prepare main function
 
